79468f36f63ac71efb50870e08e181a9fa411a1c
Jacob Loncar <jtloncar@gmail.com>
2019-02-09 10:37:28 -0700

    Added signalr ot both api and ui

---
+++ AtrophyWebApp/AtrophyWebAppAPI/Hubs/TestHub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace AtrophyWebAppAPI.Hubs
+{
+    public class TestHub : Hub
+    {
+        public void Hello(string name)
+        {
+            string message = name + ": test hub is working.";
+
+            Clients.All.hello(message);
+        }
+
+        public void TestMethodCall()
+        {
+            Hello("TestMethodCall");
+        }
+
+        public void TestMethodCallWithParams(string name)
+        {
+            Hello(name);
+        }
+    }
+}
--- AtrophyWebApp/AtrophyWebAppUI/src/app/app.module.ts
+++ AtrophyWebApp/AtrophyWebAppUI/src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { environment } from '../environments/environment';
+
+import { SignalRModule } from 'ng2-signalr';
+import { SignalRConfiguration } from 'ng2-signalr';
+
 
 import { AppComponent } from './app.component';
+
+export function createConfig(): SignalRConfiguration {
+  const c = new SignalRConfiguration();
+  c.hubName = 'TestHub';
+  //c.qs = { user: 'donald' };
+  c.url = environment.signalrURL;
+  c.logging = true;
+
+  // >= v5.0.0
+  c.executeEventsInZone = true; // optional, default is true
+  c.executeErrorsInZone = false; // optional, default is false
+  c.executeStatusChangeInZone = true; // optional, default is true
+  return c;
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SignalRModule.forRoot(createConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
+
 export class AppModule { }
